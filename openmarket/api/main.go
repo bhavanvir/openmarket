83220e6db48c99b14849d77bba3e34ac97fefcd4
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-06-02 14:49:17 -0700

    marketscrape-api -> openmarket

--- openmarket/api/main.go
+++ openmarket/api/main.go
@@ -22,6 +22,7 @@ type Request struct {
 func constructRequest(id string) string {
 	var body string
 
+	// Create a new HTTP client with the proxy settings
 	client := &http.Client{
 		Transport: &http.Transport{
 			Proxy: http.ProxyURL(&url.URL{
@@ -33,17 +34,22 @@ func constructRequest(id string) string {
 		},
 	}
 
+	// Construct the request payload
 	payload := fmt.Sprintf(`
 		&variables={
 			"targetId":"%s"
 		}
 		&doc_id=7616889011758848`, id)
 
+	// Create a new HTTP request. We also need to escape the payload in order to have a
+	// valid request body
 	req, err := http.NewRequest("POST", "https://www.facebook.com/api/graphql/", strings.NewReader(url.PathEscape(payload)))
 	if err != nil {
 		return "Error creating request"
 	}
 
+	// Set the request headers. The headers can be fiddled with to reduce the chance of being
+	// detected as a bot, but for now we'll keep it simple and just set the necessary headers
 	req.Header.Set("User-Agent", fakeuseragent.RandomUserAgent())
 	req.Header.Set("Accept", "*/*")
 	req.Header.Set("Accept-Language", "en-CA,en-US;q=0.7,en;q=0.3")
@@ -52,11 +58,6 @@ func constructRequest(id string) string {
 	req.Header.Set("Origin", "https://www.facebook.com")
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Referer", "https://www.facebook.com/marketplace/?ref=app_tab")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Priority", "u=1")
-	req.Header.Set("TE", "trailers")
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -64,6 +65,7 @@ func constructRequest(id string) string {
 	}
 	defer resp.Body.Close()
 
+	// Read the response body
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "Error reading response"
@@ -78,6 +80,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	var body string
 	statusCode := 200
 
+	// Destucture the request body
 	err := json.Unmarshal([]byte(request.Body), &req)
 	if err != nil {
 		body = "Error unmarshalling request"
@@ -85,6 +88,8 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	body = constructRequest(req.ID)
+
+	// We ran into an error if the response starts with "Error"
 	if strings.HasPrefix(body, "Error") {
 		statusCode = 500
 	}
